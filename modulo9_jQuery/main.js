a155fbec5bdf4d5f4fe02faa39b64df941bd8b26
ErickSwolkin <swolkinerick@gmail.com>
2023-03-16 21:27:46 +0100

    initial commit

---
+++ modulo9_jQuery/main.js
@@ -0,0 +1,12 @@
+$('form').on('submit', function(e){
+    e.preventDefault();
+    const novaTarefa = $('#nova-tarefa').val()
+    const novoItem = $('<li></li>');
+    $(`<li>${novaTarefa}</li>`).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+    $('#nova-tarefa').val(' ');
+
+    $('li').click(function(){
+       $(this).css("text-decoration","line-through"); 
+    })
+})
