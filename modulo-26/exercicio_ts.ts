7115fa09447f93eeca30f4f3752ec8255db86937
ErickSwolkin <swolkinerick@gmail.com>
2023-06-11 21:36:05 +0200

    initial commit

---
+++ modulo-26/exercicio_ts.ts
@@ -0,0 +1,11 @@
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2
+}
+
+function digaOla (nome: string): void {
+    console.log('Olá', `${nome}`)
+}
+
+console.log(multiplicar(2, 4))
+
+digaOla('Professor')
